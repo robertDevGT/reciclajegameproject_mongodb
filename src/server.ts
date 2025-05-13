56e0ad1d7b64e00831955cb37fa80a95ed9990b7
Jose Roberto Acan Santizo <auxtic04@legumexadt.net>
2025-05-13 08:18:56 -0600

    bugfixes(cors): agrega url del frontend a la whitelist

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import { connectDB } from "./config/db";
 import express from "express";
 import dotenv from 'dotenv';
+import cors from 'cors';
 import categoryRoutes from "./routes/categoryRoutes";
 import itemRoutes from "./routes/itemRoutes";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./config/swagger";
+import { corsConfig } from "./config/cors";
 
 dotenv.config();
 
@@ -12,12 +14,13 @@ connectDB();
 
 const app = express();
 app.use(express.json());
+app.use(cors(corsConfig));
 
 //routes
 app.use('/api/categories', categoryRoutes);
 app.use('/api/items', itemRoutes);
 
 //Docs
-app.use('/docs',swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 export default app;
