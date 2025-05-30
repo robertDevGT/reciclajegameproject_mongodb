480aaffc5dcd5f6f5531a09695a4e617245baa88
Jose Roberto Acan Santizo <auxtic04@legumexadt.net>
2025-05-30 12:25:30 -0600

    code coverage

--- src/routes/__tests__/category.test.ts
+++ src/routes/__tests__/category.test.ts
@@ -1,29 +1,29 @@
 import request from "supertest";
 import server from "../../server";
 
-// describe('POST /api/categories', () => {
-//     test('should return validation errors', async () => {
-//         const response = await request(server).post('/api/categories').send({});
+describe('POST /api/categories', () => {
+    test('should return validation errors', async () => {
+        const response = await request(server).post('/api/categories').send({});
 
-//         expect(response.status).toBe(400);
-//         expect(response.body).toHaveProperty('errors');
-//         expect(response.body.errors).toHaveLength(1);
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors).toHaveLength(1);
 
-//         expect(response.status).not.toBe(200);
-//         expect(response.body.errors).not.toHaveLength(2);
-//     });
+        expect(response.status).not.toBe(200);
+        expect(response.body.errors).not.toHaveLength(2);
+    });
 
-//     test('should create a new category', async () => {
-//         const response = await request(server).post('/api/categories').send({ "categoryName": "Categoria Test" });
+    test('should create a new category', async () => {
+        const response = await request(server).post('/api/categories').send({ "categoryName": "Categoria Test" });
 
-//         expect(response.status).toBe(201);
-//         expect(response.text).toBe('Categoria Creada Correctamente');
+        expect(response.status).toBe(201);
+        expect(response.text).toBe('Categoria Creada Correctamente');
 
-//         expect(response.status).not.toBe(500);
-//         expect(response.status).not.toBe(200);
-//         expect(response.body).not.toHaveProperty('errors');
-//     });
-// });
+        expect(response.status).not.toBe(500);
+        expect(response.status).not.toBe(200);
+        expect(response.body).not.toHaveProperty('errors');
+    });
+});
 
 describe('GET  /api/categories', () => {
     test('should check if /api/categories exists', async () => {
@@ -64,7 +64,7 @@ describe('GET  /api/categories/:id', () => {
     });
 
     test('Should return a JSON response of a Category', async () => {
-        const categoryId = '6822d4b989a21baf525ecf13';
+        const categoryId = '6839f586f7e837580f3566df';
         const response = await request(server).get(`/api/categories/${categoryId}`);
 
         expect(response.status).toBe(200);
@@ -73,80 +73,80 @@ describe('GET  /api/categories/:id', () => {
     });
 });
 
-// describe('PUT  /api/categories/:id', () => {
-//     test('Should return an error for a non valid id', async () => {
-//         const categoryId = '6822d4b989a21baf525ecf13s';
-//         const response = await request(server).put(`/api/categories/${categoryId}`).send({});
+describe('PUT  /api/categories/:id', () => {
+    test('Should return an error for a non valid id', async () => {
+        const categoryId = '6822d4b989a21baf525ecf13s';
+        const response = await request(server).put(`/api/categories/${categoryId}`).send({});
 
-//         expect(response.status).toBe(400);
-//         expect(response.body).toHaveProperty('errors');
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
 
-//         expect(response.status).not.toBe(200);
-//     });
+        expect(response.status).not.toBe(200);
+    });
 
-//     test('Should return a 404 response for a non-existent category', async () => {
-//         const categoryId = '6822d4b989a21baf525ecf14';
-//         const response = await request(server).put(`/api/categories/${categoryId}`).send({ "categoryName": "Nombre de categoria" });
+    test('Should return a 404 response for a non-existent category', async () => {
+        const categoryId = '6822d4b989a21baf525ecf14';
+        const response = await request(server).put(`/api/categories/${categoryId}`).send({ "categoryName": "Nombre de categoria" });
 
-//         expect(response.status).toBe(404);
-//         expect(response.text).toBe('Categoría no encontrada');
+        expect(response.status).toBe(404);
+        expect(response.text).toBe('Categoría no encontrada');
 
-//         expect(response.status).not.toBe(200);
-//     });
+        expect(response.status).not.toBe(200);
+    });
 
-//     test('should return validation errors', async () => {
-//         const categoryId = '6822d4b989a21baf525ecf13';
-//         const response = await request(server).put(`/api/categories/${categoryId}`).send({});
+    test('should return validation errors', async () => {
+        const categoryId = '6822d4b989a21baf525ecf13';
+        const response = await request(server).put(`/api/categories/${categoryId}`).send({});
 
-//         expect(response.status).toBe(400);
-//         expect(response.body).toHaveProperty('errors');
-//         expect(response.body.errors).toHaveLength(1);
-//     });
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors).toHaveLength(1);
+    });
 
-//     test('should update a category by categoryId', async () => {
-//         const categoryId = '6839311d37d0ab5223c969d1';
-//         const response = await request(server).put(`/api/categories/${categoryId}`).send({ "categoryName": "Categoria Test Updated" });
+    test('should update a category by categoryId', async () => {
+        const categoryId = '6839f586f7e837580f3566df';
+        const response = await request(server).put(`/api/categories/${categoryId}`).send({ "categoryName": "Categoria Test Updated" });
 
-//         expect(response.status).toBe(201);
-//         expect(response.text).toBe('Categoría Actualizada Correctamente');
+        expect(response.status).toBe(201);
+        expect(response.text).toBe('Categoría Actualizada Correctamente');
 
-//         expect(response.status).not.toBe(200);
-//         expect(response.status).not.toBe(404);
-//         expect(response.body).not.toHaveProperty('errors');
-//     });
-// });
+        expect(response.status).not.toBe(200);
+        expect(response.status).not.toBe(404);
+        expect(response.body).not.toHaveProperty('errors');
+    });
+});
 
-// describe('DELETE  /api/categories/:id', () => {
-//     test('Should return an error for a non valid id', async () => {
-//         const categoryId = '6839311d37d0ab5223c969d1s';
-//         const response = await request(server).delete(`/api/categories/${categoryId}`);
+describe('DELETE  /api/categories/:id', () => {
+    test('Should return an error for a non valid id', async () => {
+        const categoryId = '6839311d37d0ab5223c969d1s';
+        const response = await request(server).delete(`/api/categories/${categoryId}`);
 
-//         expect(response.status).toBe(400);
-//         expect(response.body).toHaveProperty('errors');
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
 
-//         expect(response.status).not.toBe(200);
-//     });
+        expect(response.status).not.toBe(200);
+    });
 
-//     test('Should return an 404 for a non-existant category', async () => {
-//         const categoryId = '6822d4b989a21baf525ecf14';
-//         const response = await request(server).delete(`/api/categories/${categoryId}`);
+    test('Should return an 404 for a non-existant category', async () => {
+        const categoryId = '6822d4b989a21baf525ecf14';
+        const response = await request(server).delete(`/api/categories/${categoryId}`);
 
-//         expect(response.status).toBe(404);
-//         expect(response.text).toBe('Categoría no encontrada');
+        expect(response.status).toBe(404);
+        expect(response.text).toBe('Categoría no encontrada');
 
-//         expect(response.status).not.toBe(200);
-//     });
+        expect(response.status).not.toBe(200);
+    });
 
 
-//     test('Should delete a Category', async () => {
-//         const categoryId = '683931250c5dda7648ef9c4b';
-//         const response = await request(server).delete(`/api/categories/${categoryId}`);
+    test('Should delete a Category', async () => {
+        const categoryId = '6839f586f7e837580f3566df';
+        const response = await request(server).delete(`/api/categories/${categoryId}`);
 
-//         expect(response.status).toBe(201);
-//         expect(response.text).toBe('Categoría Eliminada Correctamente');
+        expect(response.status).toBe(201);
+        expect(response.text).toBe('Categoría Eliminada Correctamente');
 
-//         expect(response.status).not.toBe(404);
-//         expect(response.status).not.toBe(500);
-//     });
+        expect(response.status).not.toBe(404);
+        expect(response.status).not.toBe(500);
+    });
 
-// });
+});
--- src/routes/__tests__/item.test.ts
+++ src/routes/__tests__/item.test.ts
@@ -1,56 +1,56 @@
 import request from "supertest";
 import server from "../../server";
 
-// describe('POST /api/items/:categoryId', () => {
-//     test('should return an error for non valid categoryId', async () => {
-//         const categoryId = '683931250c5dda7648ef9c4bs';
-//         const response = await request(server).post(`/api/items/${categoryId}`).send({ "itemName": "New Item", "imageUrl": "google.com" });
+describe('POST /api/items/:categoryId', () => {
+    test('should return an error for non valid categoryId', async () => {
+        const categoryId = '683931250c5dda7648ef9c4bs';
+        const response = await request(server).post(`/api/items/${categoryId}`).send({ "itemName": "New Item", "imageUrl": "google.com" });
 
-//         expect(response.status).toBe(400);
-//         expect(response.body).toHaveProperty('errors');
-//         expect(response.body.errors).toHaveLength(1);
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors).toHaveLength(1);
 
-//         expect(response.status).not.toBe(200);
-//         expect(response.body.errors).not.toHaveLength(2);
-//     });
+        expect(response.status).not.toBe(200);
+        expect(response.body.errors).not.toHaveLength(2);
+    });
 
-//      test('should return an 404 response for non-existant category', async () => {
-//         const categoryId = '6839311d37d0ab5223c969d2';
-//         const response = await request(server).post(`/api/items/${categoryId}`).send({ "itemName": "New Item", "imageUrl": "google.com" });
+     test('should return an 404 response for non-existant category', async () => {
+        const categoryId = '6839311d37d0ab5223c969d2';
+        const response = await request(server).post(`/api/items/${categoryId}`).send({ "itemName": "New Item", "imageUrl": "google.com" });
 
-//         expect(response.status).toBe(404);
-//         expect(response.text).toBe('Categoría no encontrada');
+        expect(response.status).toBe(404);
+        expect(response.text).toBe('Categoría no encontrada');
 
-//         expect(response.status).not.toBe(200);
-//     });
+        expect(response.status).not.toBe(200);
+    });
 
-//     test('should return validation errors', async () => {
-//         const categoryId = '6839311d37d0ab5223c969d1';
-//         const response = await request(server).post(`/api/items/${categoryId}`).send({});
+    test('should return validation errors', async () => {
+        const categoryId = '6839311d37d0ab5223c969d1';
+        const response = await request(server).post(`/api/items/${categoryId}`).send({});
 
-//         expect(response.status).toBe(400);
-//         expect(response.body).toHaveProperty('errors');
-//         expect(response.body.errors).toHaveLength(2);
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors).toHaveLength(2);
 
-//         expect(response.status).not.toBe(200);
-//         expect(response.body.errors).not.toHaveLength(1);
-//         expect(response.body.errors).not.toHaveLength(3);
-//     });
+        expect(response.status).not.toBe(200);
+        expect(response.body.errors).not.toHaveLength(1);
+        expect(response.body.errors).not.toHaveLength(3);
+    });
 
-//     test('should create an item', async () => {
-//         const categoryId = '6839311d37d0ab5223c969d1';
-//         const response = await request(server).post(`/api/items/${categoryId}`).send({ "itemName": "New Item", "imageUrl": "google.com" });
+    test('should create an item', async () => {
+        const categoryId = '6839f586f7e837580f3566df';
+        const response = await request(server).post(`/api/items/${categoryId}`).send({ "itemName": "New Item", "imageUrl": "google.com" });
 
-//         expect(response.status).toBe(201);
-//         expect(response.text).toBe('Item Creado Correctamente');
+        expect(response.status).toBe(201);
+        expect(response.text).toBe('Item Creado Correctamente');
 
-//         expect(response.status).not.toBe(500);
-//         expect(response.status).not.toBe(400);
-//         expect(response.status).not.toBe(404);
-//         expect(response.body).not.toHaveProperty('errors');
+        expect(response.status).not.toBe(500);
+        expect(response.status).not.toBe(400);
+        expect(response.status).not.toBe(404);
+        expect(response.body).not.toHaveProperty('errors');
 
-//     });
-// });
+    });
+});
 
 describe('GET /api/items/:categoryId', () => {
     test('should return an error for non valid categoryId', async () => {
@@ -77,7 +77,7 @@ describe('GET /api/items/:categoryId', () => {
     });
 
     test('should an array of Items', async () => {
-        const categoryId = '6839311d37d0ab5223c969d1';
+        const categoryId = '6839f586f7e837580f3566df';
         const response = await request(server).get(`/api/items/${categoryId}`);
 
         expect(response.status).toBe(200);
@@ -129,7 +129,7 @@ describe('GET /api/items/:itemId/item', () => {
     });
 
     test('should return an item', async () => {
-        const itemId = '683937076c4cb3057a1b54dd';
+        const itemId = '6822d5b789a21baf525ecf46';
         const response = await request(server).get(`/api/items/${itemId}/item`);
 
         expect(response.status).toBe(200);
@@ -178,7 +178,7 @@ describe('PUT /api/items/:itemId', () => {
     });
 
     test('should update an item', async () => {
-        const itemId = '683937076c4cb3057a1b54dd';
+        const itemId = '6839f53cc7ece0d12255d8b0';
         const response = await request(server).put(`/api/items/${itemId}`).send({ "itemName": "New Name", "imageUrl": "newImage" });
 
         expect(response.status).toBe(201);
